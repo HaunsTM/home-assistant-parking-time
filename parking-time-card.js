912f1326934ba79678c2615499e35352382e7805
HaunsTM <hans.baldetorp@knowit.se>
2024-12-25 22:35:46 +0100

    .

--- parking-time-card.js
+++ parking-time-card.js
@@ -3,46 +3,45 @@ class ParkingTimeCard extends HTMLElement {
         const entityId = this.config.entity;
         const state = hass.states[entityId];
         const timestamp = new Date(state.state);
-        const timeZone = hass.config.time_zone;
-        const localTime = new Date(timestamp.toLocaleString("en-US", { timeZone }));
+        const timeZone = this.config.time_zone;
 
-        this.updateTime(localTime);
+        this.updateTime(timestamp, timeZone);
 
         if (!this.interval) {
-            this.interval = setInterval(() => this.updateTime(localTime), 1000);
+            this.interval = setInterval(() => this.updateTime(timestamp, timeZone), 1000);
         }
     }
 
-    updateTime(startTime) {
-		const now = new Date();
-		const elapsed = now - startTime;
-		const days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
-		const hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		const minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
-		const seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+    updateTime(startTime, timeZone) {
+        const now = new Date();
+        const elapsed = now - startTime;
+        const days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
-		const options = {
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit',
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit',
-			hour12: false,
-			timeZone: this.config.time_zone
-		};
-		const localTimeString = startTime.toLocaleString(undefined, options).replace(',', '');
+        const options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+            timeZone: timeZone
+        };
+        const localTimeString = startTime.toLocaleString(undefined, options).replace(',', '');
 
-		this.innerHTML = `
-			<ha-alert title="Has been parked for" alert-type="info">
-				<div class="primary">
-					${days > 0 ? `${days}d ` : ''}${this.pad(hours)} h ${this.pad(minutes)} m ${this.pad(seconds)} s
-				</div>
-				<div class="secondary">
-					Local time: ${localTimeString}
-				</div>
-			</ha-alert>`;
-	}
+        this.innerHTML = `
+            <ha-alert title="Has been parked for" alert-type="info">
+                <div class="primary">
+                    ${days > 0 ? `${days}d ` : ''}${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}
+                </div>
+                <div class="secondary">
+                    Parked: ${localTimeString}
+                </div>
+            </ha-alert>`;
+    }
 
     pad(value) {
         return String(value).padStart(2, '0');
