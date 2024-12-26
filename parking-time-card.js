918d89c77ed1ca609d480e2d7af936b76d43e57a
HaunsTM <hans.baldetorp@knowit.se>
2024-12-26 12:05:18 +0100

    refactoring

--- parking-time-card.js
+++ parking-time-card.js
@@ -1,18 +1,34 @@
 class ParkingTimeCard extends HTMLElement {
     set hass(hass) {
         const entityId = this.config.entity;
-        const state = hass.states[entityId];
-        const timestamp = new Date(state.state);
-        const timeZone = this.config.time_zone;
 
-        this.updateTime(timestamp, timeZone);
+        const state = hass.states[entityId];
+        const parkingStartTime = new Date(state.state);
+
+        const timeZone = this.config.time_zone;
+        const locale = hass.language;
+
+        this.updateTime(parkingStartTime, locale, timeZone);
 
         if (!this.interval) {
-            this.interval = setInterval(() => this.updateTime(timestamp, timeZone), 1000);
+            this.interval = setInterval(() => this.updateTime(parkingStartTime, timeZone), 1000);
         }
     }
 
-    updateTime(startTime, timeZone) {
+    updateTime(startTime, locale, timeZone) {    
+    
+        this.innerHTML = `
+            <ha-alert title="Has been parked for" alert-type="info">
+                <div class="primary">
+                    ${elapsedTimeString(startTime, locale, timeZone)}
+                </div>
+                <div class="secondary">
+                    <ha-icon icon="mdi:parking"></ha-icon> ${parkingTimeString(startTime, locale, timeZone)}
+                </div>
+            </ha-alert>`;
+    }
+
+    elapsedTimeString(startTime, locale, timeZone) {
         const now = new Date();
         const elapsed = now - startTime;
         const days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
@@ -20,27 +36,23 @@ class ParkingTimeCard extends HTMLElement {
         const minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
+        return `${days > 0 ? `${days}d ` : ''}${this.pad(hours)} h ${this.pad(minutes)} min ${this.pad(seconds)} s`;
+    }
+
+    parkingTimeString(startTime, locale, timeZone) {
+        // Convert the timestamp to the local time in the specified time zone
         const options = {
+            timeZone: timeZone,
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
-            hour12: false,
-            timeZone: timeZone
+            hour12: false
         };
-        const localTimeString = startTime.toLocaleString(undefined, options).replace(',', '');
-
-        this.innerHTML = `
-            <ha-alert title="Has been parked for" alert-type="info">
-                <div class="primary">
-                    ${days > 0 ? `${days}d ` : ''}${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}
-                </div>
-                <div class="secondary">
-                    Parked: ${localTimeString}
-                </div>
-            </ha-alert>`;
+        
+        return startTime.toLocaleString(locale, options);
     }
 
     pad(value) {
